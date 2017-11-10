0a1c002b34711b6f51b73ee8c5a7a50eca041f98
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2017-11-10 18:25:01 +0100

    Removed printing

--- middleware/src/util/Worker.java
+++ middleware/src/util/Worker.java
@@ -214,7 +214,6 @@ public class Worker implements Runnable {
                 // Check for hits or misses.
                 response.get(barray, 0, response.limit());
                 String response_str = new String(Arrays.copyOfRange(barray, 0, response.limit())).trim();
-                System.out.println(response_str);
                 if(!response_str.equals("END") && !response_str.equals("ERROR") && !response_str.equals("SERVER_ERROR") && !response_str.equals("CLIENT_ERROR") && !response_str.equals("NOT_STORED")) {
                     request.hit = true;
                 }
